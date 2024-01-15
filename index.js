11db49badeac74c9cffa5de5ac2f65b12f994daa
Rengkat <successinmotion11@gmail.com>
2024-01-15 21:59:09 +0100

    not found route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const todos = require("./router/todos");
+const notFound = require("./middleware/not-found");
 const port = process.env.PORT || 3000;
 const app = express();
 //middlewares
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/todos", todos);
+app.use(notFound);
 app.listen(port, () => console.log(`Server listening at port ${port}`));
