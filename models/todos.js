539a762118a21fb8ac34672253d084ddae6e14c4
Rengkat <successinmotion11@gmail.com>
2024-01-16 21:04:14 +0100

    add mongoose

---
+++ models/todos.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const TodosSchema = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: [true, "Please enter task"],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+module.exports = mongoose.model(Todos, TodosSchema);
